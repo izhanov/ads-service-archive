cf580ee307224e5e4c73530b43e941e4db353d77
Izhanov <ibekable@gmail.com>
2020-05-31 11:46:35 +0600

    Rename ENV["APP_ENV"] to ENV["RACK_ENV"]

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
-ENV["APP_ENV"] ||= "development"
+ENV["RACK_ENV"] ||= "development"
 
 require "bundler"
-Bundler.setup(:default, ENV["APP_ENV"])
+Bundler.setup(:default, ENV["RACK_ENV"])
 
 require "dotenv"
-Dotenv.load(".env.#{ENV["APP_ENV"]}", ".env")
+Dotenv.load(".env.#{ENV["RACK_ENV"]}", ".env")
 
 require_relative "boot"
 Boot.setup
