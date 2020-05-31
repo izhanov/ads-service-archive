d07d6bc1d8ffa97a9aafcc1d636656f7edb9e70f
Izhanov <ibekable@gmail.com>
2020-05-31 11:48:15 +0600

    Set default locale for environments

--- config/initializer/i18n.rb
+++ config/initializer/i18n.rb
@@ -1,3 +1,4 @@
-I18n.load_path += Dir[Application.root.concat('/locales/**/*.yml')]
+I18n.load_path += Dir[Application.root.concat('config/locales/**/*.yml')]
 I18n.available_locales = %i[en ru]
-I18n.default_locale = :ru
+I18n.default_locale = :ru if ENV["RACK_ENV"] == "development"
+I18n.default_locale = :en if ENV["RACK_ENV"] == "test"
